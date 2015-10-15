9bf8e946bd70ed9160f846f45d16b32f8c4008ad
Taylor Vance <taylorvance@tvMBP14.local>
2015-10-15 06:11:14 -0500

    Firecloak now increases Fireball's damage while active.

--- Assets/Scripts/Spells/Fireball.cs
+++ Assets/Scripts/Spells/Fireball.cs
@@ -2,16 +2,18 @@ using UnityEngine;
 using System.Collections;
 
 public class Fireball : Spell {
-	public float damage;
-	public float speed;
+	public float damage, speed;
+
+	public static float multiplier = 1f;
 	
 	public override void Cast () {
+		GetComponent<Transform> ().localScale *= multiplier;
 		GetComponent<Rigidbody2D> ().velocity = GetComponent<Transform> ().up * speed;
 	}
 
 	void OnTriggerEnter2D (Collider2D other) {
 		if (other.tag == "Enemy") {
-			other.SendMessage ("Damage", damage);
+			other.SendMessage ("Damage", damage * multiplier);
 			Destroy (gameObject);
 		}
 	}
