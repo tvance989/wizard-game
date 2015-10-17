17c2bf53cb1baac742c25d957ef14dac45c73b9b
System Administrator <root@tvMBP14.local>
2015-10-17 10:14:24 -0500

    [FEATURE DELIVERS #105851990] Armyodillos spell

---
+++ Assets/Scripts/Spells/Armadillo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Armadillo : MonoBehaviour {
+	public float damage, speed;
+
+	public void Roll () {
+		GetComponent<Rigidbody2D> ().velocity = Vector3.right * speed;
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.tag == "Enemy") {
+			other.SendMessage ("Damage", damage);
+		}
+	}
+}
---
+++ Assets/Scripts/Spells/Armyodillos.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Armyodillos : Spell {
+	public int number;
+
+	public override void Cast () {GameObject[] enemies = GameObject.FindGameObjectsWithTag ("Enemy");
+		float y = Camera.main.orthographicSize;
+		float x = y * Screen.width / Screen.height;
+		for (int i = 0; i < number; i++) {
+			Vector3 pos = new Vector3 (Random.Range (-x-10f, -x), Random.Range (-y, y), 0f);
+			//.change spawn to somewhere off screen. maybe roll in from same direction or all random
+			GameObject clone = Instantiate (Resources.Load ("Spells/Armadillo") as GameObject, pos, Quaternion.identity) as GameObject;
+			clone.GetComponent<Armadillo> ().Roll ();
+		}
+
+		Destroy (this.gameObject);
+	}
+}
