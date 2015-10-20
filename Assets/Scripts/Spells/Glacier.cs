1f3facfd8ecf92e79f56da3d69db698bc873158a
Taylor Vance <taylorvance@tvMBP14.local>
2015-10-19 22:20:06 -0500

    [FEATURE DELIVERS #105848366] Glacier spell

---
+++ Assets/Scripts/Spells/Glacier.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Glacier : Spell {
+	public float dps, speed;
+	
+	public override void Cast () {
+		Transform transform = GetComponent<Transform> ();
+		transform.position = new Vector3 (Camera.main.orthographicSize * Screen.width / Screen.height + 5f, 0f, 0f);
+		transform.eulerAngles = new Vector3 (0f, 0f, 90f);
+
+		GetComponent<Rigidbody2D> ().velocity = transform.up * speed;
+	}
+	
+	void OnTriggerStay2D (Collider2D other) {
+		if (other.tag == "Enemy") {
+			other.SendMessage ("Damage", dps * Time.deltaTime);
+		}
+	}
+}
