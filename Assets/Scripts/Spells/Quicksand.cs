beb28b8c6e2d0bec3c7f0096151f1ec415240999
Taylor Vance <taylorvance@tvMBP14.local>
2015-10-28 21:16:07 -0500

    [FEATURE DELIVERS #105843832] Quicksand spell

---
+++ Assets/Scripts/Spells/Quicksand.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Quicksand: Spell {
+	public float speedMultiplier, dps, duration;
+	
+	private float ttl;
+	private static List<EnemyController> enemies = new List<EnemyController> ();
+	
+	void Start () {
+		Transform transform = GetComponent<Transform> ();
+		transform.position = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		transform.position = new Vector3 (transform.position.x, transform.position.y, 0f);
+	}
+	
+	public override void Cast () {
+		ttl = Time.time + duration;
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.tag == "Enemy") {
+			EnemyController enemy = other.GetComponent<EnemyController> ();
+
+			enemy.speedMultiplier = speedMultiplier;
+			enemies.Add (enemy);
+		}
+	}
+	
+	void OnTriggerStay2D (Collider2D other) {
+		if (other.tag == "Enemy")
+			other.gameObject.SendMessage ("Damage", dps * Time.deltaTime);
+	}
+
+	void OnTriggerExit2D (Collider2D other) {
+		if (other.tag == "Enemy")
+			other.GetComponent<EnemyController> ().speedMultiplier = 1f;
+	}
+	
+	void Update() {
+		if (Time.time >= ttl) {
+			foreach (var enemy in enemies)
+				enemy.speedMultiplier = 1f;
+
+			Destroy (gameObject);
+		}
+	}
+}
