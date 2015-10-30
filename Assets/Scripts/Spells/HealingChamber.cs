5e0980b7275021c8c48d324c0ba03dab06bf23a1
Taylor Vance <taylorvance@tvMBP14.local>
2015-10-29 21:53:14 -0500

    [FEATURE DELIVERS #105945808] HealingChamber spell

---
+++ Assets/Scripts/Spells/HealingChamber.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealingChamber: Spell {
+	public float hps, duration;
+	public GameObject explosion;
+
+	private float ttl;
+	private PlayerController player;
+
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ();
+
+		GetComponent<Transform> ().position = player.GetComponent<Transform> ().position;
+	}
+	
+	public override void Cast () {
+		player.isFrozen = player.isInvincible = true;
+		player.GetComponent<Collider2D> ().enabled = false;
+
+		ttl = Time.time + duration;
+	}
+
+	void Update() {
+		if (Time.time >= ttl) {
+			player.isFrozen = player.isInvincible = false;
+			player.GetComponent<Collider2D> ().enabled = true;
+
+			Explode ();
+		}
+
+		player.gameObject.SendMessage ("Heal", hps * Time.deltaTime);
+	}
+
+	void Explode () {
+		//.spawn explosion and damage nearby enemies
+		Instantiate (explosion, GetComponent<Transform> ().position, Quaternion.identity);
+
+		Destroy (gameObject);
+	}
+}
