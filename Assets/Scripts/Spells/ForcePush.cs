e0db064d9bdc3d73a77ae4b7430e7c9609839a6a
Taylor Vance <taylorvance@tvMBP14.local>
2015-11-12 22:02:11 -0600

    [FEATURE DELIVERS #105840598] ForcePush spell

---
+++ Assets/Scripts/Spells/ForcePush.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForcePush : Spell {
+	public float force, duration;
+
+	float ttl;
+	new Transform transform;
+	Transform player;
+
+	void Start () {
+		transform = GetComponent<Transform> ();
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform> ();
+
+		transform.Rotate (new Vector3 (0, 0, 78));
+		transform.SetParent (player);
+	}
+	
+	public override void Cast () {
+		ttl = Time.time + duration;
+	}
+	
+	void OnTriggerStay2D (Collider2D other) {
+		if (other.tag == "Enemy")
+			other.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (player.up.x, player.up.y) * force);
+	}
+	
+	void Update() {
+		if (Time.time >= ttl)
+			Destroy (gameObject);
+	}
+}
